7691eae14cc07dec58feae663abfa2b0e78c3157
banala-jwala <banalaprajwala@gmail.com>
2022-02-10 15:53:34 +0530

    my second commit

---
+++ multiple.java
@@ -0,0 +1,15 @@
+class multiple
+{
+static int multiple(int a)
+{
+System.out.println(7*a);
+return 7*a;
+}
+public static void main(String args[])
+{
+for(int i=1;i<=10;i++)
+{
+multiple(i);
+}
+}
+}
